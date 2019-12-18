8e728a7c913cb40508492516617953e3da160977
Aaron Beasley <aaroncbrs@gmail.com>
2019-12-18 14:17:32 -0600

    Completed project and Stretch goals.

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
-
+import { Route } from 'react-router-dom';
 import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
@@ -11,7 +13,10 @@ const App = () => {
 
   return (
     <div>
-      <SavedList list={savedList} />
+      <Route exact path='/' component={MovieList}/>
+      <Route exact path="/movies/:id" render={(props) => <Movie {...props}
+      addToSavedList={addToSavedList} />} />
+      <SavedList list={savedList}/>
       <div>Replace this Div with your Routes</div>
     </div>
   );
